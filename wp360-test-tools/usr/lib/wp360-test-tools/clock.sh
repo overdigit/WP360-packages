6ef13dfa4d5a3c19c73b6274ea98cc4c5ec19218
Nicola Orlando <niki.or97@yahoo.it>
2025-09-11 18:53:34 +0200

    wp360-test-tools: reduce sampling rate, reduce clutter

--- wp360-test-tools/usr/lib/wp360-test-tools/clock.sh
+++ wp360-test-tools/usr/lib/wp360-test-tools/clock.sh
@@ -1,3 +1,4 @@
 while true ; do
-	echo $(vcgencmd measure_clock arm | cut -d= -f2) $(vcgencmd get_throttled | cut -d= -f2)
+	echo $(vcgencmd measure_temp) $(vcgencmd measure_clock arm | cut -d= -f2) $(vcgencmd get_throttled | cut -d= -f2)
+	sleep 0.1
 done
